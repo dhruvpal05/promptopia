d8c0f9e07a2df90fb3e62baff4079bff798b7f79
dhruvpal05 <dhruv416pal@gmail.com>
2024-03-19 21:04:15 +0530

    auth completed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import nextAuth from "next-auth";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDatabase } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
     providers: [
@@ -9,10 +11,31 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
-    async session({session}) {
+    async session({ session }) {
+        // store the user id from MongoDB to session
+        const sessionUser = await User.findOne({ email: session.user.email });
+        session.user.id = sessionUser._id.toString();
 
+        return session;
     },
-    async signIn({profile}) {
+    async signIn({ profile }) {
+        try {
+            await connectToDatabase();
+            // Check if user exists in the database
+            const userExists = await User.findOne({ email: profile.email });
+            // If user does not exist, create a new user
+            if (!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture,
+                });
+            }
 
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
     }
 });
