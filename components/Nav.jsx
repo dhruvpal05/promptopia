057154853ffbfd0397f51064e76df0ad380c0db4
dhruvpal05 <dhruv416pal@gmail.com>
2024-03-23 19:43:33 +0530

    .

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,41 +1,44 @@
 "use client";
-
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react"
-// import { useEffect } from "react";
-
 
 function Nav() {
-    const isUserLoggedIn = true;
-
-    const [toggledropdown, settoggledropdown] = useState(false);
+    const { data: session } = useSession();
 
     const [providers, setProviders] = useState(null);
+    const [toggledropdown, settoggledropdown] = useState(false);
+
     useEffect(() => {
-        const setProviders = async () => {
-            const response = await getProviders();
-            setProviders(response);
+        const setUpProviders = async () => {
+            try {
+                const response = await getProviders();
+                setProviders(response);
+            } catch (error) {
+                console.error("Error fetching providers:", error);
+            }
         }
-        setProviders();
+        setUpProviders();
     }, []);
+
     return (
         <nav className="w-full pt-3 mb-16 flex-between">
             <Link href="/" className="flex gap-2 flex-center ">
                 <Image src="/assets/images/logo.svg" alt="promptopia logo" width={30} height={30} className="object-contain" />
                 <p className="logo_text">Promptopia</p>
             </Link>
+
             {/* Desktop Navigation */}
             <div className="hidden sm:flex">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex gap-3 md:gap-5">
                         <Link href="/create-prompt" className="black-btn">
                             Create Post
                         </Link>
                         <button type="button" onClick={signOut} className="outline-btn">Sign Out</button>
                         <Link href="/profile">
-                            <Image src="/assets/images/profile-user.png" width={25} height={25} />
+                            <Image src={session?.user.image || "/assets/images/profile-user.png"} width={25} height={25} />
                         </Link>
                     </div>
 
@@ -51,9 +54,9 @@ function Nav() {
             </div>
             {/* Mobile Navigation */}
             <div className="relative flex sm:hidden">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex">
-                        <Image src="/assets/images/profile-user.png" width={25} height={25} alt="profile" onClick={() => settoggledropdown((prev) => !prev)} />
+                        <Image src={session?.user.image || "/assets/images/profile-user.png"} width={25} height={25} alt="profile" onClick={() => settoggledropdown((prev) => !prev)} />
                         {toggledropdown && (
                             <div className="dropdown">
                                 <Link href="/profile" className="dropdown_link" onClick={() => settoggledropdown(false)}>
@@ -62,7 +65,7 @@ function Nav() {
                                 <Link href="/create-prompt" className="dropdown_link" onClick={() => settoggledropdown(false)}>
                                     Create Prompt
                                 </Link>
-                                <button type="button" className="w-full mt-5 black-btn" onClick={() => {settoggledropdown(false); signOut();}}>
+                                <button type="button" className="w-full mt-5 black-btn" onClick={() => { settoggledropdown(false); signOut(); }}>
                                     Sign Out
                                 </button>
                             </div>
@@ -76,11 +79,11 @@ function Nav() {
                             </button>
                         ))}
                     </>
-                    )}
+                )}
             </div>
 
         </nav>
     )
 }
 
-export default Nav
+export default Nav;
