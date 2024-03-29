6d9bfbc05ebc1292093f9b5e4a8b60c663b77f6c
dhruvpal05 <dhruv416pal@gmail.com>
2024-03-29 20:28:50 +0530

    fixing builed errors

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
@@ -53,7 +53,7 @@ const UpdatePrompt = () => {
   };
 
   return (
-    <Suspense fallback={<Loading />}>
+    <Suspense>
       <Form
         type='Edit'
         post={post}
